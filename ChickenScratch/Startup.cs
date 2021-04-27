44987914893b3dfb691a55f10e7f5a205c7f2e3d
Austin Wasinger <austinwasinger@gmail.com>
2021-04-26 23:09:09 -0600

    Removed UseHttpsRedirection

--- ChickenScratch/Startup.cs
+++ ChickenScratch/Startup.cs
@@ -57,7 +57,7 @@ namespace ChickenScratch
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
